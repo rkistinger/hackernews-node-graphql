95b6fa55aef7e8d68f531708696a1b8208056db4
Ross Kistinger <rkistinger12@gmail.com>
2020-04-06 21:49:09 -0500

    finish authentication

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+// Repeatable steps in a Prisma application:
+// 1) Adjust database schema => run SQL scripts
+// 2) Introspect database to update models in prisma schema => npx prisma introspect
+// 3) Generate prisma client node_module from reading prisma schema => npx prisma generate
+// 4) Interact with database via prisma client => const { PrismaClient } = require('@prisma/client')
+
 const { GraphQLServer } = require('graphql-yoga')
 const { PrismaClient } = require('@prisma/client')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const { APP_SECRET, getUserId } = require('./utils')
 
 const prisma = new PrismaClient()
 
@@ -16,10 +26,10 @@ const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: (parent, args, context, info) => {
-      return context.prisma.links.findMany()
+      return context.prisma.link.findMany()
     },
     link: (parent, args, context) => {
-      return context.prisma.links.findOne({
+      return context.prisma.link.findOne({
         where: {
           id: parseInt(args.id),
         },
@@ -27,16 +37,23 @@ const resolvers = {
     },
   },
   Mutation: {
-    post: (parent, args, context) => {
-      return context.prisma.links.create({
+    createLink: (parent, args, context) => {
+      const userId = getUserId(context)
+
+      return prisma.link.create({
         data: {
           description: args.description,
           url: args.url,
+          User: {
+            connect: {
+              id: userId,
+            },
+          },
         },
       })
     },
     updateLink: (parent, args, context) => {
-      return context.prisma.links.update({
+      return context.prisma.link.update({
         where: {
           id: parseInt(args.id),
         },
@@ -47,11 +64,84 @@ const resolvers = {
       })
     },
     deleteLink: (parent, args, context) => {
-      return context.prisma.links.delete({
+      return context.prisma.link.delete({
         where: {
           id: parseInt(args.id),
         },
       })
+    },
+    signup: async (parent, args, context, info) => {
+      // Encrypt the password
+      const password = await bcrypt.hash(args.password, 10)
+
+      // Create the user with encrypted password
+      const user = await prisma.user.create({
+        data: {
+          password,
+          email: args.email,
+          name: args.name,
+        },
+      })
+
+      // Generate a signed JWT
+      const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+      // Return an AuthPayload object
+      return {
+        token,
+        user,
+      }
+    },
+    login: async (parent, args, context, info) => {
+      // Get existing user by the email argument from the mutation
+      const user = await prisma.user.findOne({
+        where: {
+          email: args.email,
+        },
+      })
+      // Handle a non-existent email
+      if (!user) {
+        throw new Error(`No user found for email ${args.email}`)
+      }
+
+      // Check that the password argument from the mutation is valid
+      const isPasswordValid = await bcrypt.compare(args.password, user.password)
+      if (!isPasswordValid) {
+        throw new Error('Invalid password')
+      }
+
+      // Generate a signed JWT
+      const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+      // Return an AuthPayload object
+      return {
+        token,
+        user,
+      }
+    },
+  },
+  // Resolvers for scalar values can be omitted (Link.id, Link.url, User.name, User.email).
+  // Object fields need to be explicitly implemented because our GraphQL server can not infer where to get that data from.
+  Link: {
+    postedBy: (parent, args, context, info) => {
+      return context.prisma.link
+        .findOne({
+          where: {
+            id: parent.id,
+          },
+        })
+        .User()
+    },
+  },
+  User: {
+    links: (parent, args, context, info) => {
+      return prisma.user
+        .findOne({
+          where: {
+            id: parent.id,
+          },
+        })
+        .Link()
     },
   },
 }
@@ -60,9 +150,12 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: {
-    // All resolvers will have access to context.prisma
-    prisma,
+  // All resolvers have access to the returned object (useful for sharing data across files)
+  context: (request) => {
+    return {
+      ...request,
+      prisma,
+    }
   },
 })
 
--- src/schema.sql
+++ src/schema.sql
@@ -1,6 +1,18 @@
-CREATE TABLE "public"."Links" (
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+
+CREATE TABLE "public"."User" (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE "public"."Link" (
   id SERIAL PRIMARY KEY NOT NULL,
   "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
   description VARCHAR(255) NOT NULL,
-  url VARCHAR(255) NOT NULL
+  url VARCHAR(255) NOT NULL,
+  "postedBy" INTEGER,
+  FOREIGN KEY ("postedBy") REFERENCES "public"."User"(id)
 );
